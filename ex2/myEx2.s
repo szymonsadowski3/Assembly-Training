26a4003dca82c107a07780a7301c4b1826f54bbe
Szymon Sadowski <szymonsadowski3@gmail.com>
2017-05-30 22:16:19 +0200

    unnecesary comment deletion

--- ex2/myEx2.s
+++ ex2/myEx2.s
@@ -10,15 +10,6 @@
   .global generate_str_asm
   .text
 
-  # int i;
-  # if(inc) inc = 1;
-  # for(i = 0; i < n; ++i) {
-  #   s[i] = c;
-  #   c += inc;
-  # }
-  # s[n] = '\0';
-  # return s;
-
 generate_str_asm:
   CMP $0, %rcx
   JE dont_increment
@@ -54,15 +45,3 @@ generate_str_asm:
     JE break_increment
 
     JMP dont_increment_loop
-
-#char* generate_str(char* s, int c, int n, int inc)
-#s - string /rdi
-#c - znak /rsi
-#n - powtorzenia /rdx
-#inc - bool /rcx
-
-
-#   Ma generować stringa z lożonego z n znaków c. Jeśli inc = 0 znaki maja być takie same, w przeciwnym przypadku
-# znaki maja sie zwiekszać o 1. s jest buforem do zapisania wygenerowanego stringa, miejsce na niego powinno
-# być zarezerwowane w programie w C.
-# (protip: use malloc for greater good.)
